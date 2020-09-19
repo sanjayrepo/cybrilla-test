4b253ea7bf5a44368211e1a786f6f523da4b51de
sanjay_pradhan <sanjay.p@nooor.in>
2020-09-19 20:33:29 +0530

    setting of products router

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const productRoutes = require('./routes/products')
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+app.use('/products', productRoutes)
 app.get('/', (req, res) => {
     console.log("Hello world")
     res.send('Hello world')
