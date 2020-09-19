c58be728a39ecc9d02c2c5f846d16e47a1847b3d
sanjay_pradhan <sanjay.p@nooor.in>
2020-09-19 21:33:08 +0530

    basic get and post route setup

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,17 @@ const express = require('express')
 
 const router = express.Router();
 
+const products = []
 
 router.get('/', (req, res) => {
-    res.send('Hiiiiii')
+    res.send(products)
 });
+
+
+router.post('/', (req, res) => {
+    const product = req.body
+    products.push(product)
+    res.send('Product added to Mock db')
+})
 
 module.exports = router;
