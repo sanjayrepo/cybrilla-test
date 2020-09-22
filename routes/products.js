c06d13effd76e65b0894ee6895fe4bfa4c5beaa1
sanjay_pradhan <sanjay.p@nooor.in>
2020-09-22 06:35:46 +0530

    discounted Product API completed

--- routes/products.js
+++ routes/products.js
@@ -16,15 +16,47 @@ router.post('/', (req, res) => {
 })
 
 router.get('/discountedProduct', (req, res) => {
-    console.log(products)
-    let totalCount = 0;
+    let totalPrice = 0;
+    let totalAfterDiscount = 0;
+    let discount = 0
+    let finalObj = {}
 
     for (let i = 0; i < products.length; i++) {
+        let produetName = Object.keys(products[i])[0]
         let price = Object.values(products[i])[0]
-        totalCount += price
+        switch (produetName) {
+            case "A":
+                finalObj.A = price
+                if (price > 90) {
+                    discount = price - 15
+                    finalObj.discount = discount
+                }
+                break;
+            case "B":
+                finalObj.B = price
+                if (price > 40) {
+                    discount = price - 5
+                    finalObj.discount = discount
+                }
+                break;
+            case "C":
+                finalObj.C = price
+                break;
+            case "D":
+                finalObj.D = price
+                break;
+            default:
+                break;
+        }
+
+        totalPrice += price
     }
-    console.log(totalCount)
-    res.send(products)
+    if (totalPrice > 150) {
+        totalAfterDiscount = totalPrice - 20
+    }
+    finalObj.totalPrice = totalPrice
+    finalObj.totalAfterDiscount = totalAfterDiscount
+    res.send(finalObj)
 })
 
 module.exports = router;
